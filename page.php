e94794cf49fe6fa9d2a3e35d60cbc8c30ad8d878
Angel Diaz <angel.diaz@gmail.com>
2016-04-22 17:07:31 -0400

    feat(php): style

--- page.php
+++ page.php
@@ -13,6 +13,16 @@ get_header(); ?>
             <div class="container">
                 <div class="row">
                     <div class="col-xs-12">
+                        
+                    <?php
+                    /**
+                    * woocommerce_before_main_content hook.
+                    *
+                    * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+                    * @hooked woocommerce_breadcrumb - 20
+                    */
+                    do_action( 'woocommerce_before_main_content' );
+                    ?>
 			
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 <h1><?php the_title(); ?></h1>
