9d2cc43d8af910d4ef69e63183292bad969d1494
Angel Diaz <angel.diaz@gmail.com>
2016-04-22 15:21:46 -0400

    feat(php): style

--- page-front.php
+++ page-front.php
@@ -42,10 +42,22 @@ get_header(); ?>
             </div>
             
             
-            <div class="container panel">
+            <div class="container">
+                <?php /* 
                 <div class="row">
                     <div class="col-xs-12">
                         <?php mincommerce_featured_products(); ?>
+                    </div>
+                </div> 
+                */ ?>
+                
+                <div class="row">
+                    <div class="col-xs-12">
+                        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                        <p><?php the_content(); ?></p>
+                        <?php endwhile; else: ?>
+                        <p><?php _e('<h1>Vaya...</h1><br>Lo siento, no he podido encontrar lo que estabas buscando.'); ?></p>
+                        <?php endif; ?>
                     </div>
                 </div>
             </div>
