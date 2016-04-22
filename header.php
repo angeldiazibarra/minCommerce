42249fe9fc7af0ab799efea024209ba8d8689123
Angel Diaz <angel.diaz@gmail.com>
2016-04-22 11:02:38 -0400

    feat(php): cart

--- header.php
+++ header.php
@@ -50,7 +50,29 @@
                         
                         <nav class="head-menu">
                             <?php wp_nav_menu(array('theme_location' => 'header-menu')); ?>
-                        </nav>         
+                        </nav>   
+                        
+                        <?php
+global $woocommerce;
+
+// get cart quantity
+$qty = $woocommerce->cart->get_cart_contents_count();
+
+// get cart total
+$total = $woocommerce->cart->get_cart_total();
+
+// get cart url
+$cart_url = $woocommerce->cart->get_cart_url();
+
+// if multiple products in cart
+if($qty>1)
+      echo '<a href="'.$cart_url.'">'.$qty.' products | '.$total.'</a>';
+
+// if single product in cart
+if($qty==1)
+      echo '<a href="'.$cart_url.'">1 product | '.$total.'</a>';
+
+?>
                         
                     </div>
                 </div>
