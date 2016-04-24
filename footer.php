a8f0c6a54c087d166f695e177af8595ebd0f2275
Angel Diaz <angel.diaz@gmail.com>
2016-04-24 16:45:11 -0400

    feat(php): style

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
                     </div>
                     <div class="col-md-3 col-md-offset-3">
                         <img src="<?php bloginfo('stylesheet_directory'); ?>/resources/img/eudomus-120.png" alt="Logo" />
-                        <img src="<?php bloginfo('stylesheet_directory'); ?>/resources/img/payment-methods.png" alt="Pago Online Seguro" />
+                        <img src="<?php bloginfo('stylesheet_directory'); ?>/resources/img/payment-methods.png" alt="Pago Online Seguro" class="paymethods" />
                     </div>
                 </div>
             </div>
