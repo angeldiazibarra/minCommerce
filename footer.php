697699c57f63409722269cf0adf113c25a6eb285
Angel Diaz <angel.diaz@gmail.com>
2016-04-24 10:58:41 -0400

    feat(php): style

--- footer.php
+++ footer.php
@@ -1,18 +1,22 @@
         <footer>
     
-            <div class="container">
-                <div class="row">
-                    <div class="col-xs-12">
-                        <hr>
-                        <p>
-                            <a href="<?php echo site_url(); ?>" title="inicio">home</a> | 
-                            <a href="<?php echo site_url(); ?>/bio/" title="biografía">bio</a> | 
-                            <a href="<?php echo site_url(); ?>/articulos/" title="artículos">artículos</a> | 
-                            <a href="<?php echo site_url(); ?>/contacto/" title="contacto">contacto</a>
-                        </p>
+            <div class="footer-block">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <hr>
+                            <p>
+                                <a href="<?php echo site_url(); ?>" title="inicio">home</a> | 
+                                <a href="<?php echo site_url(); ?>/bio/" title="biografía">bio</a> | 
+                                <a href="<?php echo site_url(); ?>/articulos/" title="artículos">artículos</a> | 
+                                <a href="<?php echo site_url(); ?>/contacto/" title="contacto">contacto</a>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
+            
+            <div class="footer-logo">xxx</div>
     
         </footer>
 	
