6ac4785dd640566d06106ed5176bcc0dca5746a2
Angel Diaz <angel.diaz@gmail.com>
2016-04-27 12:37:28 -0400

    feat(php): style

--- functions.php
+++ functions.php
@@ -46,6 +46,19 @@ function mincommerce_cart_button() {
     echo  ' - '.$total.'</a>';
 }
 
+function woo_related_products_limit() {
+  global $product;
+	
+	$args['posts_per_page'] = 3;
+	return $args;
+}
+add_filter( 'woocommerce_output_related_products_args', 'jk_related_products_args' );
+  function jk_related_products_args( $args ) {
+	$args['posts_per_page'] = 3; // 3 related products
+	$args['columns'] = 1; // arranged in 1 columns
+	return $args;
+}
+
 add_action( 'init', 'register_my_menus' );
 
 remove_filter('the_excerpt', 'wpautop');
