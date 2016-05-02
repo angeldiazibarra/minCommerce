77936d4dc8e4c92055b7caebdd18c57f657b15d3
Angel Diaz <angel.diaz@gmail.com>
2016-05-02 18:39:08 -0400

    feat(php): facebook like box

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -113,6 +113,8 @@ get_header( 'shop' ); ?>
 		 */
 		do_action( 'woocommerce_sidebar' );
 	?>
+            
+        <div class="fb-page" data-href="https://www.facebook.com/bioconstruccion.y.permacultura/" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/bioconstruccion.y.permacultura/"><a href="https://www.facebook.com/bioconstruccion.y.permacultura/">Bioconstrucción y Permacultura</a></blockquote></div></div>
                         
         </div>    
     
