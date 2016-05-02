1fda8528446862f5e1fbb3ad790a2a2dcd59304d
Angel Diaz <angel.diaz@gmail.com>
2016-05-01 20:49:06 -0400

    feat(php): addthis

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -40,14 +40,14 @@ get_header( 'shop' ); ?>
 		<?php while ( have_posts() ) : the_post(); ?>
 
 			<?php wc_get_template_part( 'content', 'single-product' ); ?>
-
-		<?php endwhile; // end of the loop. ?>
             
             <br>
 <h4>¡Compártelo!</h4>
 
 <!-- Go to www.addthis.com/dashboard to customize your tools -->
 <div class="addthis_sharing_toolbox"></div>
+
+		<?php endwhile; // end of the loop. ?>
 
 	<?php
 		/**
