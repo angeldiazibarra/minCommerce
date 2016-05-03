65f5c8f0ceeae07a4b438f54d6a4d1835c0ca8c2
Angel Diaz <angel.diaz@gmail.com>
2016-05-03 05:41:41 -0400

    feat(php): author

--- single.php
+++ single.php
@@ -48,11 +48,11 @@
                         <hr>
                         <br>
                         <div class="row">
-                            <div class="col-sm-2 author">
+                            <div class="col-sm-3 author">
                                 <img src="<?php bloginfo('stylesheet_directory'); ?>/resources/img/adi.png" alt="Avatar" />
                             </div>
-                            <div class="col-sm-10">
-                                <b>Angel Diaz Ibarra</b>
+                            <div class="col-sm-9">
+                                <br><b>Angel Diaz Ibarra</b>
                                 <br>Soy arquitecto, emprendedor y consultor de bioconstrucción. Me gustaría conectar contigo en <a href="https://twitter.com/angeldiazibarra" title="Sígueme en Twitter">Twitter</a> y <a href="https://www.facebook.com/bioconstruccion.y.permacultura/" title="Bioconstrucción y Permacultura">Facebook</a>.
                             </div>
                         </div>
