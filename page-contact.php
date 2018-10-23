f17d1cf9f000711d04f5d446b54b0b4bdf594c83
Angel Diaz <angel.diaz@gmail.com>
2018-10-23 08:37:13 +0200

    eudomus

--- page-contact.php
+++ page-contact.php
@@ -10,7 +10,7 @@ get_header(); ?>
             
             <div class="container">
                 <div class="row">
-                    <div class="col-md-8">
+                    <div class="col-md-8 col-md-offset-2">
 			
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 <!--<h1><?php the_title(); ?></h1>-->
@@ -20,14 +20,6 @@ get_header(); ?>
 <?php endif; ?>
 
                     </div>
-                    
-                    <div class="col-md-4">
-
-                     <?php
-                    dynamic_sidebar( 'sidebarwidget' );
-                    ?>
-                                                                        
-                    </div>  
                 </div>
             </div>
             
