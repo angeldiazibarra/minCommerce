bd49bc7dfa0b3e2378d7ffeb48df5b09ce617460
Angel Diaz <angel.diaz@gmail.com>
2018-10-24 18:10:46 +0200

    eudomus

--- page-contact.php
+++ page-contact.php
@@ -39,8 +39,6 @@ include_once 'header_page.php'; ?>
                 <div class="row">
                     <div class="col-md-8 col-md-offset-2">
 			
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-<!--<h1><?php the_title(); ?></h1>-->
 <p><?php the_content(); ?></p>
 <?php endwhile; else: ?>
 <p><?php _e('<h1>Vaya...</h1><br>Lo siento, no he podido encontrar lo que estabas buscando.'); ?></p>
