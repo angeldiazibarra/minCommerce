dfd01df60928034bd46c2d4be13b2b4bc1ef2b92
Angel Diaz <angel.diaz@gmail.com>
2018-10-24 10:51:33 +0200

    eudomus

--- page.php
+++ page.php
@@ -29,7 +29,7 @@ include_once 'header_page.php'; ?>
             </div>
         </header>
 
-<?php /* the_post_thumbnail('full'); */ ?>
+<?php get_the_post_thumbnail_url(null,'full'); ?>
 		
 	<section class="main default-page">
             
