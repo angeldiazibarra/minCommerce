3c6df30684a2e50f680bc42788103d64a4c6c772
Angel Diaz <angel.diaz@gmail.com>
2018-10-24 18:38:40 +0200

    eudomus

--- page-front.php
+++ page-front.php
@@ -28,7 +28,7 @@ include_once 'header_page.php'; ?>
                 <div class="row titbox front">
                     <div class="col-xs-12">
                         <h1><?php the_title(); ?></h1>
-                        <h3>Nosotros te ayudamos a hacerlos realidad.</h3>
+                        <h3>Nosotros te ayudamos a hacerlos realidad</h3>
                     </div>
                 </div>
             </div>
