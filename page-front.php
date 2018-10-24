14c1bcf6b1af982ebc7cd425533c42d105232815
Angel Diaz <angel.diaz@gmail.com>
2018-10-24 23:05:35 +0200

    eudomus

--- page-front.php
+++ page-front.php
@@ -38,8 +38,8 @@ include_once 'header_page.php'; ?>
             <div class="container">
                 <div class="row">
                     <div class="col-md-8 col-md-offset-2">
-                        <p><h3>¿Por qué trabajar con nosotros?</h3>
-                        <p><strong>Porque tu casa lo es todo para ti</strong>.</p>
+                        <p><h3 class="center">¿Por qué trabajar con nosotros?</h3>
+                        <p class="center"><strong>Porque tu casa lo es todo para ti</strong>.</p>
                         <p>Es donde llegas después de un duro día de trabajo a relajarte y desconectar. Es donde tienes la oportunidad de compartir momentos con tu familia. Es donde comes, donde duermes y donde te sientes seguro.</p>
                         <p>Y es por eso que <strong>tu casa merece ser única</strong>, un hogar de verdad. Una vivienda bien diseñada podría marcar la diferencia entre ser la casa de tus sueños o una mala inversión.</p>
                     </div>
