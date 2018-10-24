b5f5201635d5f273cb66f0f6c33714c34343f975
Angel Diaz <angel.diaz@gmail.com>
2018-10-24 15:49:42 +0200

    eudomus

--- page-front.php
+++ page-front.php
@@ -2,22 +2,59 @@
 
 /*
 Template Name: Front Page
-*/
+ */
 
-get_header(); ?>
-                
-    <section>
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12 col-md-8 col-md-offset-2">
-                    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                    <p><?php the_content(); ?></p>
-                    <?php endwhile; else: ?>
-                    <p><?php _e('<h1>Vaya...</h1><br>Lo siento, no he podido encontrar lo que estabas buscando.'); ?></p>
-                    <?php endif; ?>
+include_once 'header_page.php'; ?>
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+        <header class="pageheader" style="background: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('<?php the_post_thumbnail_url('full'); ?>');background-position: center;background-repeat: no-repeat;background-size:cover;">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 eudomus">
+                        <a href="<?php echo site_url(); ?>" ><img src="<?php bloginfo('stylesheet_directory'); ?>/resources/img/eudomus-120-white.png" alt="eudomus logo" /></a>
+                    </div>
+
+                    <div class="col-md-8 head-menu">
+                        <?php /*
+                        <div class="cart-container">
+                            <?php mincommerce_cart_button(); ?>
+                        </div> 
+                        */ ?>                                           
+                        <?php wp_nav_menu(array('theme_location' => 'header-menu')); ?>
+                    </div>
+                </div>
+                <div class="row titbox">
+                    <div class="col-xs-12">
+                        <h1><?php the_title(); ?></h1>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </header>
+		
+	<section class="main default-page">
+            
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8">
+			
+<p><?php the_content(); ?></p>
+<?php endwhile; else: ?>
+<p><?php _e('<h1>Vaya...</h1><br>Lo siento, no he podido encontrar lo que estabas buscando.'); ?></p>
+<?php endif; ?>
 
+                    </div>
+                    
+                    <div class="col-md-4">
+
+                     <?php
+                    dynamic_sidebar( 'pageswidgets' );
+                    ?>
+                                                                        
+                    </div>  
+                </div>
+            </div>
+            
+	</section>
+						
 <?php get_footer();
