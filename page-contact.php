7ba78aa8be523081f3098413b6b25397461b52f8
Angel Diaz <angel.diaz@gmail.com>
2018-10-24 18:09:54 +0200

    eudomus

--- page-contact.php
+++ page-contact.php
@@ -4,7 +4,34 @@
 Template Name: Contact Page
  */
 
-get_header(); ?>
+include_once 'header_page.php'; ?>
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+        <header class="pageheader" style="background: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('<?php the_post_thumbnail_url('full'); ?>');background-position: center;background-repeat: no-repeat;background-size:cover;">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 eudomus">
+                        <a href="<?php echo site_url(); ?>" ><img src="<?php bloginfo('stylesheet_directory'); ?>/resources/img/eudomus-120-white.png" alt="eudomus logo" /></a>
+                    </div>
+
+                    <div class="col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-0 head-menu">
+                        <?php /*
+                        <div class="cart-container">
+                            <?php mincommerce_cart_button(); ?>
+                        </div> 
+                        */ ?>                                           
+                        <?php wp_nav_menu(array('theme_location' => 'header-menu')); ?>
+                    </div>
+                </div>
+                
+                <div class="row titbox">
+                    <div class="col-xs-12">
+                        <h1><?php the_title(); ?></h1>
+                    </div>
+                </div>
+            </div>
+        </header>
 		
 	<section class="main">
             
