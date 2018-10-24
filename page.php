25ce223b2def7716daafd9a46b1b1d1e05613021
Angel Diaz <angel.diaz@gmail.com>
2018-10-24 10:54:36 +0200

    eudomus

--- page.php
+++ page.php
@@ -10,7 +10,7 @@ include_once 'header_page.php'; ?>
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-        <header>
+        <header style="background-position: center; background-repeat: no-repeat; background-size: cover; background-image: url('<?php the_post_thumbnail_url('full'); ?>');">
             <div class="container">
                 <div class="row">
                     <div class="col-md-4 eudomus">
@@ -28,8 +28,6 @@ include_once 'header_page.php'; ?>
                 </div>
             </div>
         </header>
-
-xxx <?php the_post_thumbnail_url('full'); ?>
 		
 	<section class="main default-page">
             
