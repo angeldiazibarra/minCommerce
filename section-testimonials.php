e7d9ce718a6550790c7b46feb431d08b39a6aa1c
Angel Diaz <angel.diaz@gmail.com>
2018-10-25 19:47:31 +0200

    eudomus

--- section-testimonials.php
+++ section-testimonials.php
@@ -4,21 +4,27 @@
                 <div class="row">
                     <div class="col-xs-12 col-md-8 col-md-offset-2">
                         <div class="avatar"></div>
+                        <div>
                         <h4>Pablo V.</h4>
                         <p>Reforma de un chalet en Jaca</p>
                         <p><i class="fa fa-quote-left"></i> <i>¡Empresa totalmente recomendable! No sabíamos que teníamos tanto espacio, han aprovechado cada metro cuadrado al máximo. Estamos encantados con la profesionalidad y calidad del servicio. Hicieron la reforma de nuestra casa en los plazos y presupuesto acordados superando nuestras expectativas.</i> <i class="fa fa-quote-right"></i></p>
+                        </div>
                     </div>
                     <div class="col-xs-12 col-md-6 col-md-offset-3">
                         <div class="avatar"></div>
+                        <div>
                         <h4>Xavier P.</h4>
                         <p>Rehabilitación de una casa en Araguás</p>
                         <p><i class="fa fa-quote-left"></i> <i>Empresa seria donde las haya, de plena confianza. Cuidan los detalles al máximo y consiguen acabados muy profesionales. Ha sido un placer trabajar con ellos!</i> <i class="fa fa-quote-right"></i></p>
+                        </div>
                     </div>
                     <div class="col-xs-12 col-md-8 col-md-offset-2">
                         <div class="avatar"></div>
+                        <div>
                         <h4>María I.</h4>
                         <p>Reforma de un piso en Zaragoza</p>
                         <p><i class="fa fa-quote-left"></i> <i>Desde el principio en EUDOMUS tuvieron en cuenta todas nuestras preferencias, estamos muy contentos por la ejecución del proyecto y el resultado final. Todo el mundo que viene a casa se sorprende de cómo ha quedado.</i> <i class="fa fa-quote-right"></i></p>
+                        </div>
                     </div>
                 </div>
             </div>
