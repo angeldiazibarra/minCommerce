f6299859804f353a3a81a8b3200224ea5318fb4a
Angel Diaz <angel.diaz@gmail.com>
2018-10-25 19:24:09 +0200

    eudomus

--- section-testimonials.php
+++ section-testimonials.php
@@ -3,9 +3,9 @@
             <div class="container">
                 <div class="row">
                     <div class="col-xs-12 col-md-6 col-md-offset-3">
-                        <h4>María I.</h4>
-                        <p>Reforma de un piso en Zaragoza</p>
-                        <p><i>"Habíamos heredado un piso antiguo que necesitaba urgente una reforma y queríamos un diseño eficiente adaptado a nuestras necesidades. Desde el principio en EUDOMUS tuvieron en cuenta todas y cada una de nuestras preferencias, estamos muy contentos por la ejecución del proyecto y el resultado final. Todo el mundo que viene a casa se sorprende de cómo ha quedado."</i></p>
+                        <h4>Pablo V.</h4>
+                        <p>Reforma de un chalet en Jaca</p>
+                        <p><i>"¡Empresa totalmente recomendable! No sabíamos que teníamos tanto espacio, han aprovechado cada metro cuadrado al máximo. Estamos encantados con la profesionalidad y calidad del servicio. Hicieron la reforma de nuestra casa del Paseo de la Cantera en los plazos y presupuesto acordados superando nuestras expectativas."</i></p>
                     </div>
                     <div class="col-xs-12 col-md-6 col-md-offset-3">
                         <h4>Xavier P.</h4>
@@ -13,9 +13,9 @@
                         <p><i>"Empresa seria donde las haya, de plena confianza. Cuidan los detalles al máximo y consiguen acabados muy profesionales. Ha sido un placer trabajar con ellos!"</i></p>
                     </div>
                     <div class="col-xs-12 col-md-6 col-md-offset-3">
-                        <h4>Pablo V.</h4>
-                        <p>Reforma de un chalet en Jaca</p>
-                        <p><i>"¡Empresa totalmente recomendable! No sabíamos que teníamos tanto espacio, han aprovechado cada metro cuadrado al máximo. Estamos encantados con la profesionalidad y calidad del servicio. Hicieron la reforma de nuestra casa del Paseo de la Cantera en los plazos y presupuesto acordados superando nuestras expectativas."</i></p>
+                        <h4>María I.</h4>
+                        <p>Reforma de un piso en Zaragoza</p>
+                        <p><i>"Habíamos heredado un piso antiguo que necesitaba urgente una reforma y queríamos un diseño eficiente adaptado a nuestras necesidades. Desde el principio en EUDOMUS tuvieron en cuenta todas y cada una de nuestras preferencias, estamos muy contentos por la ejecución del proyecto y el resultado final. Todo el mundo que viene a casa se sorprende de cómo ha quedado."</i></p>
                     </div>
                 </div>
             </div>
