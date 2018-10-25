c9e109eb2be57701e822522ea5d85ed2390d59e0
Angel Diaz <angel.diaz@gmail.com>
2018-10-25 14:29:25 +0200

    eudomus

--- page-front.php
+++ page-front.php
@@ -64,7 +64,7 @@ include_once 'header_page.php'; ?>
                     <div class="col-md-10 col-md-offset-1">
                         <h3>¿Tienes un proyecto en mente? Hablemos.</h3>
                         <p><b>Ponte en contacto con nosotros para tener una reunión, gratis y sin compromiso, y hablaremos sobre cómo podemos ayudarte.</b></p>
-                        <p><a class="btn btn-secondary">contáctanos</a></p>
+                        <p><a class="btn btn-secondary" href="https://eudomus.com/contacto/">contáctanos</a></p>
                     </div>
                 </div>
             </div>
