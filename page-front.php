814844dba5607ebdf21fce304acf771202ac5d21
Angel Diaz <angel.diaz@gmail.com>
2018-10-26 19:11:38 +0200

    eudomus

--- page-front.php
+++ page-front.php
@@ -8,27 +8,29 @@ include_once 'header_page.php'; ?>
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-        <header class="pageheader" style="background: linear-gradient(rgba(0,0,0,0.25), rgba(0,0,0,0.25)), url('<?php the_post_thumbnail_url('full'); ?>');">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-4 eudomus">
-                        <a href="<?php echo site_url(); ?>" ><img src="<?php bloginfo('stylesheet_directory'); ?>/resources/img/eudomus-120-white.png" alt="eudomus logo" /></a>
+        <header class="pageheader" style="background-image:url('<?php the_post_thumbnail_url('full'); ?>');">
+            <div class="darken">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4 eudomus">
+                            <a href="<?php echo site_url(); ?>" ><img src="<?php bloginfo('stylesheet_directory'); ?>/resources/img/eudomus-120-white.png" alt="eudomus logo" /></a>
+                        </div>
+
+                        <div class="col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-0 head-menu">
+                            <?php /*
+                            <div class="cart-container">
+                                <?php mincommerce_cart_button(); ?>
+                            </div> 
+                            */ ?>                                           
+                            <?php wp_nav_menu(array('theme_location' => 'header-menu')); ?>
+                        </div>
                     </div>
 
-                    <div class="col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-0 head-menu">
-                        <?php /*
-                        <div class="cart-container">
-                            <?php mincommerce_cart_button(); ?>
-                        </div> 
-                        */ ?>                                           
-                        <?php wp_nav_menu(array('theme_location' => 'header-menu')); ?>
-                    </div>
-                </div>
-                
-                <div class="row titbox front">
-                    <div class="col-xs-12">
-                        <h1><?php the_title(); ?></h1>
-                        <h3>Nosotros te ayudamos a hacerlos realidad</h3>
+                    <div class="row titbox front">
+                        <div class="col-xs-12">
+                            <h1><?php the_title(); ?></h1>
+                            <h3>Nosotros te ayudamos a hacerlos realidad</h3>
+                        </div>
                     </div>
                 </div>
             </div>
