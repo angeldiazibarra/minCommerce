c9282ba3bafb0f3b4c55dbc292c0dba52f778345
Angel Diaz <angel.diaz@gmail.com>
2019-01-16 14:22:59 +0100

    eudomus

--- footer.php
+++ footer.php
@@ -83,7 +83,7 @@
                         </div>
                     </div>
                     
-                    <div class="alert-danger">Pago online no disponible. Por favor, <a href="https://eudomus.com/contacto/">contáctanos</a>.</div>
+                    <div class="alert alert-danger">Pago online no disponible. Por favor, <a href="https://eudomus.com/contacto/">contáctanos</a>.</div>
                     
                     <button class="tpvsend"><i class="fa fa-lock" aria-hidden="true"></i> Pago seguro 12€</button>
                     
