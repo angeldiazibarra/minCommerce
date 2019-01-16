43a13899d1e60b2740d6e78374b9aca6b5105f82
Angel Diaz <angel.diaz@gmail.com>
2019-01-16 10:24:50 +0100

    eudomus

--- development/js/scripts.js
+++ development/js/scripts.js
@@ -1,6 +1,7 @@
 $( document ).ready(function(){
     // console.log('load script');
     function showid(id){
+        console.log('showid: ' + id);
         document.getElementById(id).style.display = 'block';
     }
 });
