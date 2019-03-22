090b2700c7cd9be7cdc4d4e4aa9c45e66926763d
Angel Diaz <angel.diaz@gmail.com>
2019-03-22 02:53:22 +0100

    eudomus

--- deployprod.sh
+++ deployprod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git commit -a -m "eudomus"
 git push origin master
-sshd "cd /var/www/eudomus/wp-content/themes/minCommerce; git pull origin master; exit;"
+sshw "cd /var/www/eudomus/wp-content/themes/minCommerce; git pull origin master; exit;"
